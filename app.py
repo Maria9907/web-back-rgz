07e93adbc2c713b5555fc06a3f848eb2015ddb35
Матюшкина Мария <maria18112005@yandex.ru>
2025-11-24 21:51:58 +0700

    Создание базы данных postgres и добавление в код базы данных postgres и sqlite

--- app.py
+++ app.py
@@ -7,16 +7,46 @@ from flask import (
     make_response,
     abort,
     render_template,
+    current_app
 
 )
 import datetime
 import psycopg2
-
+import sqlite3
+from psycopg2.extras import RealDictCursor
+from os import path
 app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+
+def db_connect():
+    """Подключение к базе данных"""
+    if current_app.config['DB_TYPE'] == 'postgres':
+        conn = psycopg2.connect(
+            host='127.0.0.1',
+            database='pharmacy_matyushkina',
+            user='pharmacy_matyushkina',
+            password='123'
+        )
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+    else:
+        dir_path = path.dirname(path.realpath(__file__))
+        db_path = path.join(dir_path, "pharmacy.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+    
+    return conn, cur
+
+def db_close(conn, cur):
+    """Закрытие соединения с базой данных"""
+    conn.commit()
+    cur.close()
+    conn.close()
+
 
 @app.context_processor
 def inject_user():
